cd4f90bd532f419b2f03add4411cff53363cb903
Ralf Stubner <ralf.stubner@gmail.com>
2019-08-19 23:53:58 +0200

    Add basic FoodService functionality

--- src/main/java/me/stubner/fsapi/food/FoodService.java
+++ src/main/java/me/stubner/fsapi/food/FoodService.java
@@ -5,10 +5,14 @@ package me.stubner.fsapi.food;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.fatsecret.platform.model.CompactFood;
 import com.fatsecret.platform.model.Food;
+import com.fatsecret.platform.services.FatsecretService;
 
 /**
  * @author ralf
@@ -17,13 +21,23 @@ import com.fatsecret.platform.model.Food;
 @Service
 public class FoodService {
 
+	@Value("${FATSECRET_CONSUMER_KEY}")
+	private String key;
+	@Value("${FATSECRET_CONSUMER_SECRET}")
+	private String secret;
+
+	private FatsecretService service;
+
+	@PostConstruct
+	private void init() {
+		service = new FatsecretService(key, secret);
+	}
+
 	public List<CompactFood> search(String query) {
-		// TODO Auto-generated method stub
-		return null;
+		return service.searchFoods(query).getResults();
 	}
 
 	public Food getOne(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		return service.getFood(id);
 	}
 }
