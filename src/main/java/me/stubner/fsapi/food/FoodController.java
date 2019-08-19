32495f4fe83b535219f101a2792c5caf25db0c22
Ralf Stubner <ralf.stubner@gmail.com>
2019-08-20 00:46:46 +0200

    Add basic logging to FoodController

--- src/main/java/me/stubner/fsapi/food/FoodController.java
+++ src/main/java/me/stubner/fsapi/food/FoodController.java
@@ -5,6 +5,8 @@ package me.stubner.fsapi.food;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +24,8 @@ import com.fatsecret.platform.model.Food;
 @RestController
 public class FoodController {
 
+    private static final Logger logger = LoggerFactory.getLogger(FoodController.class);
+
 	private final FoodServiceInterface service;
 
 	@Autowired
@@ -31,11 +35,13 @@ public class FoodController {
 	
     @GetMapping("/api/v1/food")
     public List<CompactFood> search(@RequestParam(value="q") String query) {
+    	logger.info("Search for '{}'", query);
     	return service.search(query); 
     }
     
     @GetMapping("/api/v1/food/{id}")
     public Food getOne(@PathVariable Long id) {
+    	logger.info("Request for id {}", id);
     	return service.getOne(id);
     }
 
