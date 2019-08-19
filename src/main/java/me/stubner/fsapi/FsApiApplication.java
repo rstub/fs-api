74cb8619b72eabb243fb3003db5b74a2bf750937
Ralf Stubner <ralf.stubner@gmail.com>
2019-08-19 16:16:44 +0200

    Add empty Spring Boot project with Web Starter.

---
+++ src/main/java/me/stubner/fsapi/FsApiApplication.java
@@ -0,0 +1,13 @@
+package me.stubner.fsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsApiApplication.class, args);
+	}
+
+}
